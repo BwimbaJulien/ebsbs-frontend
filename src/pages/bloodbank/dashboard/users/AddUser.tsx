b2e44992c46e3e5b4e77b59b00701635eb33e5f4
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2024-10-08 08:17:52 +0200

    Add files via upload

--- src/pages/bloodbank/dashboard/users/AddUser.tsx
+++ src/pages/bloodbank/dashboard/users/AddUser.tsx
@@ -29,7 +29,7 @@ export default function AddUser() {
         </BreadcrumbList>
       </Breadcrumb>
       <div className="flex items-center justify-between">
-        <h1 className="text-lg font-semibold md:text-2xl">Add New Lab Technicians</h1>
+        <h1 className="text-lg font-semibold md:text-2xl">Add New Lab Technician</h1>
         <Button type="button" variant={'link'} onClick={() => navigate(-1)}>Go Back</Button>
       </div>
       <div
